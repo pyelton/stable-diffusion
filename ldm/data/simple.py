cb3855debe1d3ee815ecfe638b39a88db37bd6fe
pyelton <marecologist@gmail.com>
2023-01-30 13:09:54 -0800

    Fixing typo

--- ldm/data/simple.py
+++ ldm/data/simple.py
@@ -154,7 +154,7 @@ def hf_dataset(
     def pre_process(examples):
         processed = {}
         processed[image_key] = [tform(im) for im in examples[image_column]]
-        [print(torchvision.transforms.functional.get_image_num_channels(im) for im in examples[image_column]]
+        [print(torchvision.transforms.functional.get_image_num_channels(im)) for im in examples[image_column]]
         processed[caption_key] = examples[text_column]
         return processed
 
