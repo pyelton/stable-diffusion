b4295b6851810c53f4158149c9e1a672830b8366
pyelton <marecologist@gmail.com>
2023-02-06 16:38:56 -0800

    Increased number of steps to save checkpoint to 5000

--- main.py
+++ main.py
@@ -811,7 +811,7 @@ if __name__ == "__main__":
                          "filename": "{epoch:06}-{step:09}",
                          "verbose": True,
                          'save_top_k': -1,
-                         'every_n_train_steps': 1000,
+                         'every_n_train_steps': 5000,
                          'save_weights_only': True
                          # 'every_n_train_steps': 10000,
                      }
